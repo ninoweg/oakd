133ec929da6d2865ebafe860c8d40293263a1809
Nino Wegleitner <wegleitner.nino@gmx.at>
2023-04-24 13:45:21 +0200

    add labelMap

--- include/common.hpp
+++ include/common.hpp
@@ -7,3 +7,15 @@ namespace oakd
     const std::string CAMERA_INFO_TOPIC = "/oakd/color/preview/camera_info";
     const std::string MARKER_TOPIC = "/oakd/marker_publisher/markers";
 }
+
+static const std::vector<std::string> labelMap = {
+    "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+    "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag",
+    "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+    "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon",
+    "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza",
+    "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor",
+    "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink",
+    "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+};
